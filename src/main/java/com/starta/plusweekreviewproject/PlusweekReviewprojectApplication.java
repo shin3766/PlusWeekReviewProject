c3933ab2a03819e1d85a6e98cca358be9a82ad4f
Yoosup Shin <brian96shin@gmail.com>
2023-12-13 13:08:49 +0900

    init commit

---
+++ src/main/java/com/starta/plusweekreviewproject/PlusweekReviewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.starta.plusweekreviewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusweekReviewprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusweekReviewprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starta/plusweekreviewproject/PlusweekReviewprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starta.plusweekreviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusweekReviewprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
