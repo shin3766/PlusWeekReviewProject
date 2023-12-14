437106371c4ce812a4bea9558905a9ef223b3e2a
Yoosup Shin <brian96shin@gmail.com>
2023-12-14 20:05:39 +0900

    feat: CheckUsernameRequestDto에 정규표현식 추가

--- src/main/java/com/starta/plusweekreviewproject/dto/CheckUsernameRequestDto.java
+++ src/main/java/com/starta/plusweekreviewproject/dto/CheckUsernameRequestDto.java
@@ -1,8 +1,10 @@
 package com.starta.plusweekreviewproject.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class CheckUsernameRequestDto {
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,10}$")
     private String username;
 }
